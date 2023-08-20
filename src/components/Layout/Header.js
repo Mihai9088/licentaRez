2d3b069e9a164e59825e769441fcb9b3e8371ea9
Mihai Voina <mihaitza1058@yahoo.com>
2023-08-20 16:34:20 +0300

    build

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import {Fragment} from 'react'
-import bgImage from '../../assets/bg.jpg'
-import classes from './Header.module.css'
-import HeaderCartButton from './HeaderCartButton'
+import React from "react";
+import { Fragment } from "react";
+import bgImage from "../../assets/bg.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Electronics Shop</h1>
-            <HeaderCartButton onClick ={props.onShowCart}/>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={bgImage}/>
-        </div>
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Math Shop</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={bgImage} />
+      </div>
     </Fragment>
+  );
+};
 
-}
-
-export default Header
+export default Header;
